1246b331eb2688694003510c350ff3ad5b566c51
RuslanKharisov <ruslan.kharisov@gmail.com>
2023-01-24 15:13:20 +0300

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import "./styles/main.css";
-
-
-
 import Navbar from "./components/navbar/Navbar";
-import Header from "./components/header/header";
 import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Contacts from "./pages/Contacts";
+
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
       <Navbar />
-
-      <Header />
-
-      <main className="section">
-        <div className="container">
-
-          <ul className="content-list">
-            <li className="content-list__item">
-              <h2 className="title-2">Frontend</h2>
-              <p>JavaScript, TypeScript, ReactJS, Angular, Redux, HTML, CSS, NPM, BootStrap, MaterialUI, Yarn, TailwindCSS, StyledComponents</p>
-            </li>
-            <li className="content-list__item">
-              <h2 className="title-2">Backend</h2>
-              <p>NodeJS, MySQL, MongoDB, PHP, Laravel</p>
-            </li>
-          </ul>
-
-        </div>
-      </main>
-
-      <Footer/>
+      {/* <Home /> */}
+      <Projects />
+      {/* <Contacts /> */}
+      <Footer />
     </div>
   );
 }
